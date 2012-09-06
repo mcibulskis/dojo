e882225e620ea0bb631e84be5f188884a1f35465
DJ Daugherty <ddaugher@gmail.com>
2012-09-05 21:22:11 -0400

    checking in something that does not work, but at least deals with the sequences

--- potter/src/main/scala/v1/PriceCalculator.scala
+++ potter/src/main/scala/v1/PriceCalculator.scala
@@ -1,32 +1,40 @@
 package v1
 
+import collection.mutable.ListBuffer
+
 class PriceCalculator {
 
   def calculatePrice(books: Seq[Book]): Double = {
-    createSubsets(books).foldLeft(0) {
-      calcualteSubsetPrice(_)
-    }
+    val sequence = createSubsets(books)
+    0.0
+
+//    val x = sequence.foldLeft(0)((r, c) => calculateSubsetPrice(sequence))
+//    x
   }
 
   def createSubsets(books: Seq[Book]): Seq[Seq[Book]] = {
+    var buffer = new ListBuffer[Seq[Book]]
     val seq1 = books.distinct.toList
-    val seq2 = books.toList - seq1
-    List(seq1, seq2)
+    buffer += seq1
+    val seq2 = books.toList -- seq1
+    buffer += seq2
+
+    buffer.toList
   }
 
   def calculateSubsetPrice(books: Seq[Book]): Double = {
     val basePrice = calculateBasePrice(books)
 
     (books.distinct.size) match {
-      case(5) => basePrice * .75
-      case(4) => basePrice * .80
-      case(3) => basePrice * .90
-      case(2) => basePrice * .95
-      case(_) => basePrice
+      case (5) => basePrice * .75
+      case (4) => basePrice * .80
+      case (3) => basePrice * .90
+      case (2) => basePrice * .95
+      case (_) => basePrice
     }
   }
 
-  private def calculateBasePrice(books: scala.Seq[Book]): Double = {
+  private def calculateBasePrice(books: Seq[Book]): Double = {
     8.00 * books.size
   }
 }
