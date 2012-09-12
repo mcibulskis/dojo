3e47b3bb6349b59702adeb54af557a2459991fbb
Michael Cibulskis <mcibulskis@pillartechnology.com>
2012-09-12 12:17:35 -0400

    removed redPencil..

--- redPencil/build.sbt
+++
@@ -1,8 +0,0 @@
-name := "redPencil"
-
-version := "1.0"
-
-scalaVersion := "2.9.1"
-
-libraryDependencies += "org.scalatest" %% "scalatest" % "1.8" % "test"
-
--- redPencil/src/main/scala/v1/RedPencil.scala
+++
@@ -1,7 +0,0 @@
-package v1
-
-class RedPencil {
-  def isRedPencilPromotion(lastPrice: Double, newPrice: Double, daysLastPriceWasEffective: Int): Boolean = {
-    true
-  }
-}
