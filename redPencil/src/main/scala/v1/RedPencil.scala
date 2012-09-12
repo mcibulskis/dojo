bc6f2c471c5acea91318e424a2034e0c92712f97
Michael Cibulskis <mcibulskis@pillartechnology.com>
2012-09-12 12:28:32 -0400

    adding back in first part of implementation

--- redPencil/src/main/scala/v1/RedPencil.scala
+++ redPencil/src/main/scala/v1/RedPencil.scala
@@ -2,6 +2,7 @@ package v1
 
 class RedPencil {
   def isRedPencilPromotion(lastPrice: Double, newPrice: Double, daysLastPriceWasEffective: Int): Boolean = {
+    if (daysLastPriceWasEffective < 30) return false
     true
   }
 }
