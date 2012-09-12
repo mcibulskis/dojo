cadcebb7c6692084964511de3e8bbfb0db1ef70b
Michael Cibulskis <mcibulskis@pillartechnology.com>
2012-09-12 11:56:59 -0400

    failing test for not enough days for last price

---
+++ redPencil/src/test/scala/v1/RedPencilSpec.scala
@@ -0,0 +1,14 @@
+package v1
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+
+class RedPencilSpec extends FlatSpec with ShouldMatchers {
+  val impl = new RedPencil
+
+  behavior of "Determining if a price reduction should be flagged as a red pencil promotion"
+
+  it should "return false if the last price was effective for fewer than 30 days" in {
+    impl.isRedPencilPromotion(12.00, 10.00, 29) should equal(false)
+  }
+}
